c4a79ed7e41e886b4798e77099d2e92e5fd2b218
Nandakishore VV <vnandub@gmail.com>
2022-12-12 00:52:58 +0530

    api setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # movie-bubble-ui
+# an application that displays a movie carousal by taking a list of movies from a public API
--- movie-bubble/src/App.js
+++ movie-bubble/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
+//c9ce3514 api key
 
-function App() {
+const API_URL = 'http://www.omdbapi.com?apikey=c9ce3514'
+
+const App = ()=>{
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`)
+    const data = await response.json()
+    console.log(data)
+  }
+  useEffect(()=>{
+    searchMovies("Batman")
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Hello World</h1>
   );
 }
 
