2079500694373240d00cee12e0e8f6c8ac20f849
Nandakishore VV <vnandub@gmail.com>
2022-12-22 00:02:20 +0530

    added movie cards

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # movie-bubble-ui
 # an application that displays a movie carousal by taking a list of movies from a public API
+
+# To take a build, 
+# npm run build 
+# this will create a production build
+
+# https://www.copycat.dev/blog/reactjs-build-production/
+# https://www.freecodecamp.org/news/how-to-build-a-react-project-with-create-react-app-in-10-steps/
--- movie-bubble/src/App.js
+++ movie-bubble/src/App.js
@@ -1,26 +1,65 @@
 import './App.css';
 import SearchIcon from './search.svg'
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
 //c9ce3514 api key
 
 const API_URL = 'http://www.omdbapi.com?apikey=c9ce3514'
 
-const App = ()=>{
+const App = () => {
+
+  const [movies, setMovies] = useState([])
+  const [search, setSearch] = useState("")
+
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`)
     const data = await response.json()
-    console.log(data)
+    setMovies(data.Search)
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     searchMovies("Batman")
-  },[])
+  }, [])
+
 
   return (
-      <div className="app">
-        <h1>Movie Bubble</h1>
+
+    <div className="app">
+      <h1>Movie Bubble</h1>
+      <div className='search'>
+        <input
+          placeholder='Search For Movies'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt='searchIcon'
+          onClick={() => searchMovies(search)}
+        />
       </div>
+
+      {
+        movies?.length > 0
+          ? (
+          <div className='container'>
+            {movies.map((movie)=>(
+              <MovieCard movie={movie}/>
+            ))}
+          </div>
+          )
+          :
+          (
+            <div>
+              <h2>No Movies Found</h2>
+            </div>
+          )
+      }
+
+
+
+    </div>
   );
 }
 
